65658857cd93d3da2966b22303658cc8e9e9297d
vnothing97-dev <vnothing97@gmail.com>
2026-05-26 21:03:06 -0600

    Add files via upload

---
+++ BoelTiersWeb/script.js
@@ -0,0 +1,7 @@
+// Solo para interactividad básica
+document.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('click', () => {
+        document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+    });
+});
